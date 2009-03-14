7d1a6a28bc4332c0f53655cb3d82333692cc0f2d
krokodil <krokodil@4a487464-c306-11dd-87c2-1b6a6bee664f>
2009-03-14 22:23:30 +0000

    more tests
    git-svn-id: http://php-amqplib.googlecode.com/svn/trunk@18 4a487464-c306-11dd-87c2-1b6a6bee664f

--- tests/wire_format_test.php
+++ tests/wire_format_test.php
@@ -21,16 +21,23 @@ class AMQPWriterTests extends UnitTestCase
         $w->write_longlong('0');
         $this->assertEqual($w->getvalue(),"\0\0\0\0\0\0\0\0");
 
+        // First test with values represented as strings
         $this->longlongWriteAndRead('0');
         $this->longlongWriteAndRead('123');
         $this->longlongWriteAndRead('4294967296');
         $this->longlongWriteAndRead('994294967296');
+
+        // Now, with real int values
+        $this->longlongWriteAndRead(0);
+        $this->longlongWriteAndRead(123);
+        $this->longlongWriteAndRead(4294967296);
     }
 
     function longlongWriteAndRead($v)
     {
         $w = new AMQPWriter();
         $w->write_longlong($v);
+        
         $r = new AMQPReader($w->getvalue());
         $this->assertEqual($r->read_longlong(),$v);
     }
